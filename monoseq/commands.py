eedb83798704a4ba615d6ef61dd408940387c833
Martijn Vermaat <martijn@vermaat.name>
2013-07-13 13:37:48 +0200

    Custom block length and blocks per line from command line

--- monoseq/commands.py
+++ monoseq/commands.py
@@ -43,25 +43,31 @@ def _fasta_iter(fasta):
         yield header, sequence
 
 
-def _pprint_fasta(fasta, annotations=None):
+def _pprint_fasta(fasta, annotations=None, block_length=10,
+                  blocks_per_line=6):
     """
     Pretty-print each record in the FASTA file.
     """
     annotations = annotations or []
     for header, sequence in _fasta_iter(fasta):
         print header
-        print pprint_sequence(sequence, annotations=annotations, mode='ansi')
+        print pprint_sequence(sequence, annotations=annotations,
+                              block_length=block_length,
+                              blocks_per_line=blocks_per_line, mode='ansi')
 
 
-def _pprint_line(line, annotations=None):
+def _pprint_line(line, annotations=None, block_length=10, blocks_per_line=6):
     """
     Pretty-print one line.
     """
     annotations = annotations or []
-    print pprint_sequence(line, annotations=annotations, mode='ansi')
+    print pprint_sequence(line, annotations=annotations,
+                          block_length=block_length,
+                          blocks_per_line=blocks_per_line, mode='ansi')
 
 
-def pprint(sequence_file, annotation=None):
+def pprint(sequence_file, annotation=None, block_length=10,
+           blocks_per_line=6):
     """
     Pretty-print sequence(s) from a file.
     """
@@ -72,9 +78,12 @@ def pprint(sequence_file, annotation=None):
         line = next(sequence_file)
         if line.startswith('>'):
             _pprint_fasta(itertools.chain([line], sequence_file),
-                          annotations=annotations)
+                          annotations=annotations, block_length=block_length,
+                          blocks_per_line=blocks_per_line)
         else:
-            _pprint_line(line.strip(), annotations=annotations)
+            _pprint_line(line.strip(), annotations=annotations,
+                         block_length=block_length,
+                         blocks_per_line=blocks_per_line)
     except StopIteration:
         pass
 
@@ -89,13 +98,19 @@ def main():
                         default=sys.stdin, type=argparse.FileType('r'),
                         help='file to read sequence(s) from, can be in FASTA '
                         'format (default: standard input)')
+    parser.add_argument('-b', metavar='INT', dest='block_length', type=int,
+                        default=10, help='block length (default: 10)')
+    parser.add_argument('-l', metavar='INT', dest='blocks_per_line', type=int,
+                        default=6, help='blocks per line (default: 6)')
     parser.add_argument('-a', metavar='POS', dest='annotation', nargs=2,
                         action='append', type=int, help='first and last '
                         'positions of subsequence to annotate (allowed more '
                         'than once)')
 
     args = parser.parse_args()
-    pprint(_until_eof(args.sequence_file), annotation=args.annotation)
+    pprint(_until_eof(args.sequence_file), annotation=args.annotation,
+           block_length=args.block_length,
+           blocks_per_line=args.blocks_per_line)
 
 
 if __name__ == '__main__':
